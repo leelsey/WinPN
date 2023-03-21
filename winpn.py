a2fc7e492421307c78b8776d8a856a92110a4712
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2023-03-22 06:24:44 +0900

    Add all Windows' proxy function

--- winpn.py
+++ winpn.py
@@ -1,56 +1,85 @@
 import argparse, winreg
 
-
 DEFAULT_AUTO_PROXY_URL = "http://0.0.0.0:8080" # Default address for the automatic proxy, it's sample
 DEFAULT_PROXY_SERVER = "127.0.0.1:8080" # Default address for the proxy server, it's sample
-INTERNET_SETTINGS = r'Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+INTERNET_SETTINGS = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings'
 REG_PROXY_KEY = winreg.OpenKey(winreg.HKEY_CURRENT_USER, INTERNET_SETTINGS, 0, winreg.KEY_ALL_ACCESS)
-AUTO_PROXY_KEY = 'AutoConfigURL'
-PROXY_SERVER_KEY = 'ProxyServer'
+DETECT_PROXY_KEY = 'AutoDetect'
+SCRIPT_PROXY_KEY = 'AutoConfigURL'
 PROXY_ENABLE_KEY = 'ProxyEnable'
+PROXY_SERVER_KEY = 'ProxyServer'
 
 
-def toggle_auto_proxy(status, address):
+def toggle_detect_proxy(status):
     if status:
-        winreg.SetValueEx(REG_PROXY_KEY, AUTO_PROXY_KEY, 0, winreg.REG_SZ, address)
+        winreg.SetValueEx(REG_PROXY_KEY, DETECT_PROXY_KEY, 0, winreg.REG_DWORD, 1)
+        print("enablea automatically detect proxy")
     else:
-        winreg.DeleteValue(REG_PROXY_KEY, AUTO_PROXY_KEY)
+        winreg.SetValueEx(REG_PROXY_KEY, DETECT_PROXY_KEY, 0, winreg.REG_DWORD, 0)
+        print("disabled automatically detect proxy")
     winreg.CloseKey(REG_PROXY_KEY)
+    exit()
+
+
+def toggle_script_proxy(status, address):
+    if status:
+        winreg.SetValueEx(REG_PROXY_KEY, SCRIPT_PROXY_KEY, 0, winreg.REG_SZ, address)
+        print("enabled automatic proxy script to: " + address)
+    else:
+        try:
+            winreg.DeleteValue(REG_PROXY_KEY, SCRIPT_PROXY_KEY)
+            print("disabled automatic proxy script")
+        except FileNotFoundError:
+            print("already disabled automatic proxy script")
+    winreg.CloseKey(REG_PROXY_KEY)
+    exit()
 
 
 def toggle_proxy_server(status, address):
     if status:
         winreg.SetValueEx(REG_PROXY_KEY, PROXY_ENABLE_KEY, 0, winreg.REG_DWORD, 1)
         winreg.SetValueEx(REG_PROXY_KEY, PROXY_SERVER_KEY, 0, winreg.REG_SZ, address)
+        print("enabled proxy server to: " + address)
     else:
-        winreg.SetValueEx(REG_PROXY_KEY, PROXY_ENABLE_KEY, 0, winreg.REG_DWORD, 0)
+        if winreg.QueryValueEx(REG_PROXY_KEY, PROXY_ENABLE_KEY)[0] == 0:
+            print("already disabled proxy server")
+        else:
+            winreg.SetValueEx(REG_PROXY_KEY, PROXY_ENABLE_KEY, 0, winreg.REG_DWORD, 0)
+            print("disabled windows proxy")
     winreg.CloseKey(REG_PROXY_KEY)
+    exit()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--auto", choices=['on', 'off'], help="Enable/disable automatic proxy configuration")
-    parser.add_argument("-s", "--server", choices=['on', 'off'], help="Enable/disable manual proxy server")
+    parser.add_argument("-d", "--detect", choices=['on', 'enable', 'e', 'off', 'disable', 'd'], help="Enable/disable automatically detect proxy")
+    parser.add_argument("-s", "--script", choices=['on', 'enable', 'e', 'off', 'disable', 'd'], help="Enable/disable setup script proxy")
+    parser.add_argument("-m", "--manual", choices=['on', 'enable', 'e', 'off', 'disable', 'd'], help="Enable/disable manual proxy server")
     parser.add_argument("-l", "--localproxy", nargs=1, metavar="Localhost Proxy", help="Enable manual proxy server with localhost")
-    parser.add_argument("-m", "--modify", nargs=1, metavar="Address", help="Address for the automatic proxy or the server proxy")
+    parser.add_argument("-a", "--address", nargs=1, metavar="Address", help="Address for the automatic proxy or the server proxy")
     args = parser.parse_args()
 
-    if args.auto == 'on':
-        if args.modify:
-            proxy_path = args.modify[0]
+    if args.detect == 'on' or args.detect == 'enable' or args.detect == 'e':
+        toggle_detect_proxy(True)
+    elif args.detect == 'off' or args.detect == 'disable' or args.detect == 'd':
+        toggle_detect_proxy(False)
+
+    if args.script == 'on' or args.script == 'enable' or args.script == 'e':
+        if args.address:
+            proxy_path = args.address[0]
         else:
             proxy_path = DEFAULT_AUTO_PROXY_URL
-        toggle_auto_proxy(True, proxy_path)
-    elif args.auto == 'off':
-        toggle_auto_proxy(False, None)
+        toggle_script_proxy(True, proxy_path)
+    elif args.script == 'off' or args.script == 'disable' or args.script == 'd':
+        toggle_script_proxy(False, None)
 
-    if args.server == 'on':
-        if args.modify:
-            proxy_path = args.modify[0]
+    if args.manual == 'on' or args.manual == 'enable' or args.manual == 'e':
+        if args.address:
+            proxy_path = args.address[0]
         else:
             proxy_path = DEFAULT_PROXY_SERVER
         toggle_proxy_server(True, proxy_path)
-    elif args.server == 'off':
+    elif args.manual == 'off' or args.manual == 'disable' or args.manual == 'd':
         toggle_proxy_server(False, None)
 
     if args.localproxy:
